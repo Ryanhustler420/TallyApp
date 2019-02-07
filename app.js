793a8f47de2c26d8c90009290503f8bff9e708a4
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-08 01:20:35 +0530

    Reading Input Data and Refactor DOMstrings

--- app.js
+++ app.js
@@ -6,22 +6,47 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
   // write code here...
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector (DOMstrings.inputType).value, // will be either 'inc' || 'exp'
+        description: document.querySelector (DOMstrings.inputDescription).value,
+        value: document.querySelector (DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 }) ();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
   // write code here...
 
+  var DOM = UICtrl.getDOMstrings ();
+
   var ctrlAddItem = function () {
     // TODO LIST
     // 1. Get the field input data
+    var input = UICtrl.getInput ();
+    console.log (input);
+
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
-    // 5. Display the budget on the UI    
+    // 5. Display the budget on the UI
   };
 
-  document.querySelector ('.add__btn').addEventListener ('click', ctrlAddItem);
+  document.querySelector (DOM.inputBtn).addEventListener ('click', ctrlAddItem);
 
   document.addEventListener ('keypress', function (e) {
     if (e.key === 'Enter' && e.keyCode === 13) {
