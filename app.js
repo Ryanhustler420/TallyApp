4843e93b0cb1d7a1d2e79071f0ec36523c262dd5
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-10 13:32:41 +0530

    Update the budget: UI controller

--- app.js
+++ app.js
@@ -66,8 +66,7 @@ var budgetController = (function () {
       // calculate the % of income that we spent
       if (data.totals.inc > 0)
         data.percentage = Math.round (data.totals.exp / data.totals.inc * 100);
-      else 
-        data.percentage = -1;
+      else data.percentage = -1;
     },
     getBudget: function () {
       return {
@@ -94,6 +93,10 @@ var UIController = (function () {
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
     expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage',
   };
 
   return {
@@ -141,6 +144,20 @@ var UIController = (function () {
 
       fieldsArray[0].focus ();
     },
+    displayBudget: function (obj) {
+      document.querySelector (DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector (DOMstrings.incomeLabel).textContent =
+        obj.totalInc;
+      document.querySelector (DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector (DOMstrings.percentageLabel).textContent =
+          obj.percentage + '%';
+      } else {
+        document.querySelector (DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -174,7 +191,8 @@ var controller = (function (budgetCtrl, UICtrl) {
     budget = budgetCtrl.getBudget ();
 
     // 3. Display the budget on the UI
-    console.log (budget);
+    // console.log (budget);
+    UICtrl.displayBudget (budget);
   };
 
   var ctrlAddItem = function () {
@@ -202,6 +220,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       console.log ('Application has started');
+      UICtrl.displayBudget ({
+        budget: 0,
+        percentage: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
       setupEventListener ();
     },
   };
