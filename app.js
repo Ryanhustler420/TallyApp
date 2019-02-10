7dcc3b2d18c6ed09fea6288c06a8be9dfc76ca6f
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-10 15:05:10 +0530

    event delegation
    Setting up the delete event listener using event delegation

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ var UIController = (function () {
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
+    container: '.container', // for event bubling
   };
 
   return {
@@ -114,10 +115,10 @@ var UIController = (function () {
       // Create STML string with placeholder text
       if (type === 'inc') {
         element = DOMstrings.incomeContainer;
-        html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+        html = `<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
       } else if (type === 'exp') {
         element = DOMstrings.expensesContainer;
-        html = `<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+        html = `<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
       }
 
       // Replace the placeholder text with some actual data
@@ -179,6 +180,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         ctrlAddItem ();
       }
     });
+    document
+      .querySelector (DOM.container)
+      .addEventListener ('click', ctrlDeleteItem);
   };
 
   var updateBudget = function () {
@@ -214,6 +218,24 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       // 5. Calculate And Update Budget
       updateBudget ();
+    }
+  };
+
+  var ctrlDeleteItem = function (event) {
+    var itemID, splitID, type, ID;
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      // inc-0, inc-2
+      splitID = itemID.split ('-');
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. delete the item from the dara structure
+
+      // 2. Delete the item from the UI
+
+      // 3. Update and show the new budget
     }
   };
 
